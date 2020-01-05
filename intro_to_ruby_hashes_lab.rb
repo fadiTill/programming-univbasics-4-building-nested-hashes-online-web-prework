9d67f7db72b9b32cf6ef011e397552aef76716ee
fadimata tillman <fadi.tillman@gmail.com>
2020-01-05 19:54:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,26 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  base_hash = {                            
+    :base_first_categorie => {
+      "undercategorieone" => {
+        :categoriea => "undercategoriea"
+        :categorieb => "undecategorieb"
+      },
+      "undercategorietwo" => { 
+        :categoriea => "undercategorieatwo"
+        :categorieb => "undercategoriebtwo"
+    }
+    },
+    :base_second_categorie => {
+      "undercategoriethree" => {
+        :categoriea => "undercategorieathree"
+        :categorieb => "undercategoriebthree"
+      },
+      "undercategoriefour" => {
+        :categoriea => "undercategorieafour"
+        :categorieb => "undercategoriebfour"
+      }
+    }
+  }
 end
 
 def monopoly_with_second_tier
