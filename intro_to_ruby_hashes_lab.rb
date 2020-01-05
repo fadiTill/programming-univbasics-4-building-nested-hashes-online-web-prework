d5f26fb23fccd9f5ad499b4c8f621f5dcdadad06
fadimata tillman <fadi.tillman@gmail.com>
2020-01-05 20:00:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,6 +39,22 @@ end
 
 def monopoly_with_third_tier
   monopoly_with_third_tier = {
+    :base_first_categorie => {
+      "undercategorieone" => {
+        :categoriea => "undercategoriea"
+        :categorieb => "undecategorieb"
+      },
+      "undercategorietwo" => { 
+        :categoriea => "undercategorieatwo"
+        :categorieb => "undercategoriebtwo"
+    }
+    },
+    :base_second_categorie => {
+      "undercategoriethree" => {
+        :categoriea => "undercategorieathree"
+        :categorieb => "undercategoriebthree"
+      },
+  }
 end
 
 def monopoly_with_fourth_tier
